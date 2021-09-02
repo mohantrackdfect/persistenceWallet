819daa3e01062eb7d373df7a3579b5d86082aac5
blackpanther8132 <raju@persistence.one>
2021-09-03 00:01:53 +0530

    fixing txn issu

--- src/constants/url.js
+++ src/constants/url.js
@@ -7,7 +7,6 @@ export const getValidatorsUrl = () => `${API_URL}/cosmos/staking/v1beta1/validat
 export const getValidatorUrl = (address) => `${API_URL}/cosmos/staking/v1beta1/validators/${address}`;
 export const getBalanceUrl = (address) => `${API_URL}/cosmos/bank/v1beta1/balances/${address}`;
 export const getTxnUrl = (hash) => `${API_URL}/cosmos/tx/v1beta1/txs/${hash}`;
-
 export const getSendTransactionsUrl  = (address , limit, pageNumber) =>`${API_URL}/cosmos/tx/v1beta1/txs?events=message.sender='${address}'&order_by=ORDER_BY_DESC&pagination.offset=${limit * pageNumber}&pagination.limit=${limit}&pagination.count_total=true`;
 export const getReceiveTransactionsUrl  = (address , limit, pageNumber) =>`${API_URL}/cosmos/tx/v1beta1/txs?events=transfer.recipient='${address}'&order_by=ORDER_BY_DESC&pagination.offset=${limit * pageNumber}&pagination.limit=${limit}&pagination.count_total=true`;
 export const getValidatorRewardsUrl = (address, validatorAddress) => `${API_URL}/cosmos/distribution/v1beta1/delegators/${address}/rewards/${validatorAddress}`;
